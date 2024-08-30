2bc438a643d468133340901fda06f1c5e58862a9
Joshua Pabilona <joshua@ockham.ai>
2024-08-30 23:25:27 +0800

    Ignore extra key value pairs in configs

--- src/config.py
+++ src/config.py
@@ -21,31 +21,45 @@ class Settings(BaseSettings):
     5. The default field values for the Settings model.
     """
 
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(
+        env_file=".env", env_file_encoding="utf-8", extra="ignore"
+    )
 
     openai_dev_service_account: str
     openai_project_id: str
     openai_model_name: str
 
-    api_key: str
+    api_key: str = 'foo'
 
 
 class PostgresSettings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=".env-postgres", env_file_encoding="utf-8"
+        env_file=".env-postgres", env_file_encoding="utf-8", extra="ignore"
     )
 
-    postgres_dsn: PostgresDsn = None
     postgres_dbname: str
     postgres_host: str
     postgres_user: str
     postgres_password: str
     postgres_host: str
-    postgres_port: str
+    postgres_port: int
+
+    @property
+    def postgres_dsn(self) -> PostgresDsn:
+        return PostgresDsn.build(
+            scheme="postgresql",
+            username=self.postgres_user,
+            password=self.postgres_password,
+            host=self.postgres_host,
+            port=self.postgres_port,
+            path=f"/{self.postgres_dbname}",
+        )
 
 
 class QdrantSettings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env-qdrant", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(
+        env_file=".env-qdrant", env_file_encoding="utf-8", extra="ignore"
+    )
 
     qdrant_host: str
     qdrant_rest_api_port: str
@@ -68,3 +82,8 @@ def get_database_settings():
 @lru_cache()
 def get_vector_database_settings():
     return QdrantSettings()
+
+
+if __name__ == '__main__':
+    postgres_settings = get_database_settings()
+    print(f"{postgres_settings.postgres_dsn=}")
